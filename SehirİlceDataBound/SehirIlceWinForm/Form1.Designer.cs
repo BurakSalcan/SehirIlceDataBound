7679d9e5b1ad6adcda84456cfe804669445f34a5
Burak SALCAN <buraksalcan@gmail.com>
2024-11-10 12:44:34 +0300

    SehirIlceDataBound
    9.11.2024 dersi

---
+++ SehirİlceDataBound/SehirIlceWinForm/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SehirIlceWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_sehirler = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_ilceler = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cb_sehirler
+            // 
+            this.cb_sehirler.FormattingEnabled = true;
+            this.cb_sehirler.Location = new System.Drawing.Point(178, 18);
+            this.cb_sehirler.Name = "cb_sehirler";
+            this.cb_sehirler.Size = new System.Drawing.Size(262, 24);
+            this.cb_sehirler.TabIndex = 0;
+            this.cb_sehirler.Text = "Şehir Seçiniz...";
+            this.cb_sehirler.SelectedIndexChanged += new System.EventHandler(this.cb_sehirler_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Şehir Seçiniz :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(43, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "İlçe Seçiniz : ";
+            // 
+            // cb_ilceler
+            // 
+            this.cb_ilceler.FormattingEnabled = true;
+            this.cb_ilceler.Location = new System.Drawing.Point(178, 62);
+            this.cb_ilceler.Name = "cb_ilceler";
+            this.cb_ilceler.Size = new System.Drawing.Size(262, 24);
+            this.cb_ilceler.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 450);
+            this.Controls.Add(this.cb_ilceler);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_sehirler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_sehirler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_ilceler;
+    }
+}
+
